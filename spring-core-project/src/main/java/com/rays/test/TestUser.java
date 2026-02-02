d16912a8517a33c2d584bcc82e18c3de5ca69196
Rishabh Shrivastava <rishabhshrivastava7565@gmail.com>
2026-02-02 17:17:03 +0530

    done

---
+++ spring-core-project/src/main/java/com/rays/test/TestUser.java
@@ -0,0 +1,35 @@
+package com.rays.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class TestUser {
+	
+	public static void main(String[] args) {
+		
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("user.xml"));
+		
+	   User dto = (User) factory.getBean("user");
+	   System.out.println(dto.getName());
+	   System.out.println(dto.getAddress());
+	   
+	   System.out.println("---------------------------");
+	   
+	    dto = (User) factory.getBean("user1");
+	   System.out.println(dto.getName());
+	   System.out.println(dto.getAddress());
+	   
+	   System.out.println("----------------------------");
+	   
+	   
+	   dto = (User) factory.getBean("user2");
+	   System.out.println(dto.getName());
+	   System.out.println(dto.getAddress());
+		
+		
+		
+		
+	}
+
+}
---
+++ spring-core-project/src/main/java/com/rays/test/User.java
@@ -0,0 +1,33 @@
+package com.rays.test;
+
+public class User {
+
+	private String name;
+	private String address;
+
+	public User() {                  //Default constructor
+
+	}
+
+	public User(String name, String address) {      //Parameterized constructor
+		this.name = name;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
